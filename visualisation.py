303bde0080ac684f13f28fa03c9ac0821d7cc0b9
Timo <timo.antoniak@btb.gmbh>
2025-07-09 18:20:10 +0200

    new UI and visualisation

---
+++ visualisation.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
+def plot_with_query(vectors, query_vector, highlight_indices=None):
+    all_embeddings = np.vstack([vectors, query_vector])
+
+    pca = PCA(n_components=2)
+    reduced_all = pca.fit_transform(all_embeddings)
+
+    reduced_embeddings = reduced_all[:-1]
+    query_tsne = reduced_all[-1]
+    
+    plt.figure(figsize=(8, 5))
+    # Plot all points
+    plt.scatter(reduced_embeddings[:, 0], reduced_embeddings[:, 1], s=10, label="Alle Datenpunkte")
+    # Highlight found points if provided
+    if highlight_indices is not None and len(highlight_indices) > 0:
+        plt.scatter(reduced_embeddings[highlight_indices, 0], reduced_embeddings[highlight_indices, 1], 
+                    s=80, c="orange", marker="o", edgecolor="black", linewidths=1.5, label="Gefundene Punkte")
+    # Plot query vector
+    plt.scatter(query_tsne[0], query_tsne[1], s=200, c="red", marker="*", edgecolor="black", linewidths=1.5, label="Query-Vektor")
+    plt.title("Visualisierung der Embeddings mit PCA")
+    plt.legend()
+    plt.show()
